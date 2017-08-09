0bc47656ac6f35908e971a415a964441d2dda5e4
xiexuanzheng123 <349328984@qq.com>
2017-08-09 15:28:01 +0800

    first demo：

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './App.css';
+
+class App extends React.Component {
+    render() {
+        return(
+            <h1 className={style.title}>Hello World</h1>
+        )
+    }
+}
+export default App;
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import App from './components/App.js';
+
+render(
+  <App />,
+  document.getElementById("index")
+);

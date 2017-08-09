88b5036c0140de45514e54d0e481a657873320e2
xiexuanzheng123 <349328984@qq.com>
2017-08-09 18:59:56 +0800

    the inherit of css

--- components/App.js
+++ components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     render() {
         return(
             <div>
-                <h1 className="title">Hello World</h1>
+                <h1 className={style.title}>Hello World</h1>
                 <h2 className={style.h2}>Test</h2>
             </div>
         )

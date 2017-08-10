f7ba62d5867a050d6f8950a8e4719176f15b0864
xiexuanzheng123 <349328984@qq.com>
2017-08-10 17:16:55 +0800

    the usage of @nest

---
+++ components/Nestings.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import nestings from './nestings.css';
+
+class Nestings extends Component {
+    render() {
+        return(
+            <div>
+                <div className={nestings.foo}>nestings.foo
+                    <div className={nestings.bar}>nestings.bar</div>
+                </div>
+                <div className={nestings.parent}>
+                   <div className={nestings.foo}>
+                        nestings.parent + ' ' + nestings.foo
+                   </div>
+                </div>
+            </div>
+            
+        );
+    }
+}
+export default Nestings;

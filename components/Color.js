7f064156da0a2b7d51ad3ae0ee7a28e2847402a5
xiexuanzheng123 <349328984@qq.com>
2017-08-10 18:02:16 +0800

    add color-function

---
+++ components/Color.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import color from '../css/color.css';
+
+class Color extends Component {
+    render() {
+        return (
+            <div>
+                <a>这是对color的一个测试</a>
+            </div>
+        );
+    }
+}
+export default Color;
